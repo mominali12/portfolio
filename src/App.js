771a6f7ff1f6c3021fdeead31e14ca17daa5a001
mominali12 <mominali1296@gmail.com>
2019-11-17 01:29:42 +0500

    under-construction prompt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src={
+            "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwip0pDdyO_lAhUnDWMBHXk9C68QjRx6BAgBEAQ&url=http%3A%2F%2Fwww.netanimations.net%2FFlashing-Under-Construction-Signs-Barricade-Animationed-gifs.htm&psig=AOvVaw3V9P1YjhK4DKYMFdi_o18n&ust=1574022416885924"
+          }
+          className="App-logo"
+          alt="logo"
+        />
+        <p>Under Construction !!!</p>
       </header>
     </div>
   );
