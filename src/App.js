2fc80ffc07e69f9fb1c2292e8611d21851e58c9f
mominali12 <mominali1296@gmail.com>
2019-11-17 01:35:51 +0500

    Underconstruction logo added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
+import logo from "./logo.gif";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img
-          src={
-            "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwip0pDdyO_lAhUnDWMBHXk9C68QjRx6BAgBEAQ&url=http%3A%2F%2Fwww.netanimations.net%2FFlashing-Under-Construction-Signs-Barricade-Animationed-gifs.htm&psig=AOvVaw3V9P1YjhK4DKYMFdi_o18n&ust=1574022416885924"
-          }
-          className="App-logo"
-          alt="logo"
-        />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>Under Construction !!!</p>
       </header>
     </div>
